c10804c8209236f1efffa10eca1a5d513120fd36
teranmaleesha <teranmaleesha@gmail.com>
2026-09-20 13:11:15 +0530

    Add files via upload

---
+++ Week1/interpreter.py
@@ -0,0 +1,15 @@
+expression = input("Expression: ").strip()
+x,y,z = expression.split(" ")
+x = float(x)
+z = float(z)
+if y == "+":
+    result = x + z
+elif y == ("-"):
+    result = x - z
+elif y == ("*"):
+    result = x * z
+elif y ==("/"):
+    result = x / z
+print(f"{result:.1f}")
+
+
